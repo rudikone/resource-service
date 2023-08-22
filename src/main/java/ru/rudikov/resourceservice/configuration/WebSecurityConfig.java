48d2076c4d0d93ce074558feec4fce14cba9ac32
e.rudikov <e.rudikov@tinkoff.ru>
2023-08-22 21:09:00 +0300

    chore: add logging to web filter chain

--- src/main/java/ru/rudikov/resourceservice/configuration/WebSecurityConfig.java
+++ src/main/java/ru/rudikov/resourceservice/configuration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.rudikov.resourceservice.configuration;
 
 import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHENTICATION;
 import static ru.rudikov.resourceservice.application.domain.model.auth.jwt.Role.ADMIN;
 
@@ -8,12 +9,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.FormLoginSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.HttpBasicSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 import ru.rudikov.resourceservice.application.service.auth.JwtFilter;
 
 @RequiredArgsConstructor
@@ -24,31 +27,22 @@ public class WebSecurityConfig {
 
   private final JwtFilter jwtFilter;
 
-  // почему сразу две ошибки и статус не ставится?
   @Bean
   public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
     http.httpBasic(HttpBasicSpec::disable)
         .formLogin(FormLoginSpec::disable)
         .csrf(CsrfSpec::disable)
-        //        .exceptionHandling(
-        //            exceptionHandlingSpec -> {
-        //              exceptionHandlingSpec.accessDeniedHandler(
-        //                  (exchange, denied) -> {
-        //                    ServerHttpResponse response = exchange.getResponse();
-        //                    response.setStatusCode(FORBIDDEN);
-        //                    response.setComplete();
-        //                    log.error(denied.getMessage());
-        //                    return Mono.empty();
-        //                  });
-        //              exceptionHandlingSpec.authenticationEntryPoint(
-        //                  (exchange, ex) -> {
-        //                    ServerHttpResponse response = exchange.getResponse();
-        //                    response.setStatusCode(UNAUTHORIZED);
-        //                    response.setComplete();
-        //                    log.error(ex.getMessage());
-        //                    return Mono.empty();
-        //                  });
-        //            })
+        .exceptionHandling(
+            exceptionHandlingSpec ->
+                exceptionHandlingSpec.authenticationEntryPoint(
+                    (exchange, ex) -> {
+                      log.error(ex.getMessage());
+                      return Mono.fromRunnable(
+                          () -> {
+                            ServerHttpResponse response = exchange.getResponse();
+                            response.setStatusCode(UNAUTHORIZED);
+                          });
+                    }))
         .authorizeExchange(
             authorizeExchangeSpec ->
                 authorizeExchangeSpec
